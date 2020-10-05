267c2e6bf0eb937946d4aa26052265e9156fe895
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-05 16:29:43 +0530

    Updates

---
+++ precache-manifest.f7153a8f977aa1b8186a30256ec49124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814c4dd57d79e125cc4e853336c1154d",
+    "url": "/botmoji/index.html"
+  },
+  {
+    "revision": "60f6f31e8968f92e9d8e",
+    "url": "/botmoji/static/css/main.78ff8561.chunk.css"
+  },
+  {
+    "revision": "8049e2ce2f939b8f677b",
+    "url": "/botmoji/static/js/2.a400fc03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/botmoji/static/js/2.a400fc03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f6f31e8968f92e9d8e",
+    "url": "/botmoji/static/js/main.a5be4adc.chunk.js"
+  },
+  {
+    "revision": "fa1a31d56355120f8172",
+    "url": "/botmoji/static/js/runtime-main.ebb16850.js"
+  }
+]);
