ce4db579b94b769dd518df3af65b9f976245f08c
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-05 14:28:54 +0530

    Initialize app

---
+++ frontend/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import './index.css';
+import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+        <BrowserRouter>
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
+        </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
