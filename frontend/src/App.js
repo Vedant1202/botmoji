a98cfc9b0d571a911d31b89a61d63b7d3ed36e09
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-05 14:29:10 +0530

    Finish adding in components and routing

---
+++ frontend/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+// import logo from './logo.svg';
+import './App.css';
+import ErrorPage from './pages/errorpage/errorpage.component';
+import AppPage from './pages/apppage/apppage.component';
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentMessages } from './redux/messages/messages.selector';
+import { connect } from 'react-redux';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentWillMount() {
+    if (localStorage.getItem('snap-user')) {
+      this.setState({
+        ...this.state,
+        currentUser: JSON.parse(localStorage.getItem('snap-user')),
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+        <Route
+          exact
+          path='/'
+          render={() =>
+            this.state.currentUser ? (
+              <AppPage currentUser={this.state.currentUser}/>
+              ) : (
+                <Redirect to='/login'></Redirect>
+            )
+          }
+        >
+        </Route>
+          <Route
+          exact
+          path='/login'
+          render={() =>
+              this.state.currentUser ? (
+                  <Redirect to='/'></Redirect>
+              ) : (
+                  <></>
+              )
+          }
+          ></Route>
+          <Route path='*' component={ErrorPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = createStructuredSelector({
+  currentMessages: selectCurrentMessages,
+});
+
+export default connect(mapStateToProps)(App);
