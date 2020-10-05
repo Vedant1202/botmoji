f05e2cfec64c3bb2d05846732b358f11fdc8572e
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-05 17:57:00 +0530

    Updated to https url

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    rasaUrl: 'http://35.192.205.244:5005',
+    rasaUrl: 'https://www.botmoji.tk:5005',
     // Use this for local development
     // rasaUrl: 'http://127.0.0.1:5005',
 }
