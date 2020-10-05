6df061feb9621fc13d9479157155837b9b409d2c
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-05 14:27:46 +0530

    Add error page

---
+++ frontend/src/pages/errorpage/errorpage.component.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from 'react';
+
+import './errorpage.styles.css';
+
+const ErrorPage = () => {
+    return (
+        <div id='main'>
+            <div className='fof'>
+                <h1>Error 404: Page not found...</h1>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
