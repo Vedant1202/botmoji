c37184ba48bb84dac4da470707c2170e645760b8
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-05 22:32:57 +0530

    Add support for a sample user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import AppPage from './pages/apppage/apppage.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentMessages } from './redux/messages/messages.selector';
 import { connect } from 'react-redux';
+import { config } from './config/config';
 
 class App extends React.Component {
 
@@ -20,12 +21,26 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    if (localStorage.getItem('snap-user')) {
+    if (config.sampleUser) {
       this.setState({
         ...this.state,
-        currentUser: JSON.parse(localStorage.getItem('snap-user')),
+        currentUser: {
+          bitmoji: {
+            avatar: "https://sdk.bitmoji.com/render/panel/387a5703-cfea-44fe-b9cd-dd6c17deee78-ATJlblZ69l5wWWa_uUr7OkWlzTn4xQ-v1.png?transparent=1&palette=1",
+          },
+          displayName: "Vedant N",
+          externalId: "CAESIMOqWFT06I8v1ymgKE/YhCJgnmBuUP0v6kwO74e9+XJs",
+        },
       });
+    } else {
+      if (localStorage.getItem('snap-user')) {
+        this.setState({
+          ...this.state,
+          currentUser: JSON.parse(localStorage.getItem('snap-user')),
+        });
+      }
     }
+    
   }
 
   render() {
--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,4 +2,5 @@ export const config = {
     rasaUrl: 'http://app.botmoji.tk:5005/rasa',
     // Use this for local development
     // rasaUrl: 'http://127.0.0.1:5005',
+    sampleUser: true, // Set to false to disable sample user
 }
