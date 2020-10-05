3e91a9fabff10b1e38c5f1a124226b3df3081618
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-05 16:39:11 +0530

    Update to urls

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,5 @@
 export const config = {
-    rasaUrl: 'http://localhost:5005',
+    rasaUrl: 'http://35.192.205.244:5005',
+    // Use this for local development
+    // rasaUrl: 'http://127.0.0.1:5005',
 }
